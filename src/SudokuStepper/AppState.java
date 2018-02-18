e4ee7f314d9812bf907a2e545bef0a6d80e9ce50
pascal <pascal.melix@gmx.de>
2018-02-18 18:19:01 +0100

    Add AppState class

---
+++ src/SudokuStepper/AppState.java
@@ -0,0 +1,36 @@
+package SudokuStepper;
+/**
+ * 
+ */
+
+/**
+ * @author Pascal
+ *
+ */
+
+public enum AppState
+{
+    EMPTY(0), CREATING(1), OPENING(2), SOLVING(3);
+    private final int val;
+
+    private AppState(int value)
+    {
+        val = value;
+    }
+
+    private static AppState[] vals = null;
+
+    public static AppState from(int i)
+    {
+        if (AppState.vals == null)
+        {
+            AppState.vals = AppState.values();
+        }
+        return AppState.vals[i - 1];
+    }
+
+    public int val()
+    {
+        return (val);
+    }
+}
