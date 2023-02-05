83c7a7f021e2ce8b7c2fddf0665aee9eca0b86d6
pascal <pascal.melix@gmx.de>
2023-02-05 20:45:33 +0100

    Towards 4x4 Sudokus

--- src/SudokuStepper/LegalValues.java
+++ src/SudokuStepper/LegalValues.java
@@ -7,10 +7,19 @@ package SudokuStepper;
  * @author Pascal
  *
  */
-public enum LegalValues
+public enum LegalValues // implements LegalValuesInterface
 {
     ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9);
-    private final int val;
+
+    private final int        val;
+    private static final int RECTANGLELENGTH     = 3;
+    private static final int CANDIDATESNUMBER    = 9;
+    private static final int CANDIDATESPERROW    = 3;
+    private static final int CANDIDATESPERCOL    = 3;
+    private static final int SINGLESUDOKUMAXROWS = 9;
+    private static final int SINGLESUDOKUMAXCOLS = 9;
+    private static final int CELLSPERROW         = 3;
+    private static final int CELLSPERCOL         = 3;
 
     private LegalValues(int value)
     {
@@ -32,9 +41,44 @@ public enum LegalValues
     {
         return (val);
     }
-    // public Object clone() throws CloneNotSupportedException
-    // {
-    // LegalValues.from(value)
-    // return (super.clone());
-    // }
+
+    public static int getCandidatesNumber()
+    {
+        return (CANDIDATESNUMBER);
+    }
+
+    public static int getRectangleLength()
+    {
+        return (RECTANGLELENGTH);
+    }
+
+    public static int getCandidatesPerRow()
+    {
+        return (CANDIDATESPERROW);
+    }
+
+    public static int getCandidatesPerCol()
+    {
+        return (CANDIDATESPERCOL);
+    }
+
+    public static int getSingleSudokuMaxRows()
+    {
+        return (SINGLESUDOKUMAXROWS);
+    }
+
+    public static int getSingleSudokuMaxCols()
+    {
+        return (SINGLESUDOKUMAXCOLS);
+    }
+
+    public static int getCellsPerRow()
+    {
+        return (CELLSPERROW);
+    }
+
+    public static int getCellsPerCol()
+    {
+        return (CELLSPERCOL);
+    }
 }
